907c716e6e3e4d64e8f6075441e918b972e9104e
SLAVA <SLAVA@SLAVAPC>
2017-07-10 17:26:33 +0600

    Исправлены замечания: некоторые методы помечены как static, хотя вызываются из объекта

--- src/variantb/main/Runner.java
+++ src/variantb/main/Runner.java
@@ -18,19 +18,21 @@ public class Runner {
     public static void main(String[] args) {
        System.out.println("Введите несколько целых чисел и нажмите <Enter>:");
        Action action = new Action();
-        int[] masNum=action.Numbers();
-        if (masNum!=null) {
-            action.ChetnNechetNumbers(masNum); //четные и нечетные числа
+        int[] masNum = action.numbers();
+        if (masNum!= null) {
+            action.chetnNechetNumbers(masNum); //четные и нечетные числа
             action.minMaxNumbers(masNum); // наибольшее и наименьшее число
             action.divisionThreeNine(masNum); //Числа, которые делятся на 3 или на 9
             action.divisionFiveSeven(masNum); //Числа, которые делятся на 5 и на 7.
             action.sortNumbers(masNum); //Элементы, расположенные методом пузырька по убыванию модулей.
             action.uniqueThreeDigit(masNum);//Все трехзначные числа, в десятичной записи которых нет одинаковых цифр
-            action.GreatestCommonDivisor(masNum); //Наибольший общий делитель 
+            action.greatestCommonDivisor(masNum); //Наибольший общий делитель 
             action.findLeastCommonMultiple(masNum); //наименьшее общее кратное этих чисел.
             action.findPrimeNumbers(masNum); //Простые числа
+       } else {
+              System.out.println("Вы не ввели числа!");
         }
-       
+    
        
              
     }
